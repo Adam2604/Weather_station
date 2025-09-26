eae352780528e3c40ade3eb80bd01a217fc523ed
Adam2604 <adammichala11@gmail.com>
2025-09-26 12:31:50 +0200

    Dostosowanie pod dht22

--- esp32_code/esp32_code.ino
+++ esp32_code/esp32_code.ino
@@ -1,8 +1,9 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "DHT.h"
 
 // WiFi configuration
-const  char* ssid = "NETGEAR60";
+const char* ssid = "NETGEAR60";
 const char* password = "aquaticpotato892";
 
 // konfiguracja MQTT
@@ -11,20 +12,19 @@ const char* mqtt_server = "192.168.0.50"; // Raspberry Pi IP
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// noise sensor pin
-#define SOUND_PIN 34
+// DHT configuration
+#define DHTPIN 13        // pin, do którego podłączony jest DATA z DHT22
+#define DHTTYPE DHT22   // typ czujnika
+DHT dht(DHTPIN, DHTTYPE);
 
-
-void setup_wifi()
-{
+void setup_wifi() {
   delay(10);
   Serial.println();
   Serial.print("Łączenie z ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -35,16 +35,12 @@ void setup_wifi()
   Serial.println(WiFi.localIP());
 }
 
-void reconnect()
-{
-  while(!client.connected())
-  {
+void reconnect() {
+  while (!client.connected()) {
     Serial.print("Łączenie z MQTT...");
-    if(client.connect("ESP32Client"))
-    {
+    if (client.connect("ESP32Client")) {
       Serial.println("Połączono!");
-    } else
-    {
+    } else {
       Serial.print("błąd, rc=");
       Serial.print(client.state());
       Serial.println("Ponawianie za 3s");
@@ -53,30 +49,43 @@ void reconnect()
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-
+  dht.begin();
 }
 
-void loop()
-{
-  if(!client.connected())
-  {
+void loop() {
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  // odczyt z czujnika hałasu
-  int sound_value = analogRead(SOUND_PIN);
-  Serial.print("Hałas: ");
-  Serial.println(sound_value);
 
-  // sending by MQTT
-  char msg[10];
-  sprintf(msg, "%d", sound_value); // funkcja sprintf zapisuje wynik do zmiennej msg, "%d" to format, zmienia na liczbę całkowitą
-  client.publish("stacja", msg);
-  delay(5000);
+  // odczyt temperatury i wilgotności
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Błąd odczytu z DHT22!");
+    return;
+  }
+
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print("°C  Wilgotność: ");
+  Serial.print(h);
+  Serial.println("%");
+
+  // wysyłanie temperatury
+  char tempMsg[10];
+  dtostrf(t, 6, 2, tempMsg);  // (wartość, szerokość, miejsca po przecinku, bufor)
+  client.publish("stacja/temperatura", tempMsg);
+
+  // wysyłanie wilgotności
+  char humMsg[10];
+  dtostrf(h, 6, 2, humMsg);
+  client.publish("stacja/wilgotnosc", humMsg);
+
+  delay(5000); // co 5 sekund
 }
-
